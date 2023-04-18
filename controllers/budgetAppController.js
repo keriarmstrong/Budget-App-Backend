d47d8820448d35d45dc1b883bf14aea4f56fa489
keriarmstrong <khetto@live.com>
2023-04-17 21:54:13 -0400

    functional delete request done

--- controllers/budgetAppController.js
+++ controllers/budgetAppController.js
@@ -7,7 +7,7 @@ const budgetData = require('../models/budgetData')
 budget.get('/',(req, res) =>{
     res.status(202).json(budgetData)
 })
-//Get Show
+//Get Show 
 budget.get('/:id', (req, res) => {
     const {id} = req.params
     const transaction = budgetData[id]
@@ -19,6 +19,16 @@ budget.post('/',(req, res) => {
     const newTransaction = req.body;
     budgetData.push(newTransaction)
     res.status(202).json(budgetData)
+})
+
+//Delete -- looking into refactoring with tenary
+budget.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    const removedTransaction = budgetData.splice(id, 1)
+    {budgetData[id] ? res.status(202).json(removedTransaction):
+    res.status(404).json({error: `Transaction with ID: ${id} Not found.`})
+    }
+    
 })
 
 
